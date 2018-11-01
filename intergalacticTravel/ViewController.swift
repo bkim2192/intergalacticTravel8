78cd9b6dbdd671751d4eeacd76e90730ffdcd61e
Brandon Kim <bkim2192@jh-mba158-16.local>
2018-11-01 14:49:42 -0500

    Initial Commit

---
+++ intergalacticTravel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  intergalacticTravel
+//
+//  Created by Brandon Kim on 11/1/18.
+//  Copyright © 2018 Brandon Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
